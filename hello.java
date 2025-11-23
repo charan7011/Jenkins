33178bf46a92e4cf0fb9b6979a67e9430436e58e
charan7011 <85923155+charan7011@users.noreply.github.com>
2025-11-23 17:10:24 +0530

    Update hello.java

---
+++ hello.java
@@ -0,0 +1,13 @@
+public class VariablesExample {
+    public static void main(String[] args) {
+        int age = 30;
+        double price = 19.99;
+        String name = "Alice";
+        boolean isLoggedIn = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Logged In: " + isLoggedIn);
+    }
+}
