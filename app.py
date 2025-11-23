60c20701f7cb8790e1220cdac27d3a95ce9b2952
charan7011 <85923155+charan7011@users.noreply.github.com>
2025-11-23 22:47:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,6 @@ sum = num1 + num2
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 
 
-Hello
+Hello welcome to amazon
 This is the commit to check whether poll scm is working or not
+
